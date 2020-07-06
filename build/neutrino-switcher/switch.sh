dc1c5da3f33e2829a899a90de02cf46515ad8b0f
nolim1t <hello@nolim1t.co>
2020-07-06 16:05:29 +0700

    Add error handling

--- build/neutrino-switcher/switch.sh
+++ build/neutrino-switcher/switch.sh
@@ -24,27 +24,35 @@ while true; do
     echo "If set to neutrino then lets check"
 
     INFO=`curl --user lncm:$PASSWORD --data-binary '{"jsonrpc": "1.0", "id":"switchme", "method": "getblockchaininfo", "params": [] }' $JSONRPCURL 2>/dev/null`
+    # check for errors
+    ERROR=`echo $INFO | jq .error`
+    if [ ! -z $ERROR ]; then
+      # if no errors
+      # Check prune mode
+      PRUNE_MODE=`echo $INFO | jq .result.pruned`
+      # check headers
+      HEADERS=`echo $INFO | jq .result.headers`
+      # check blocks
+      BLOCKS=`echo $INFO | jq .result.blocks`
 
-    # Check prune mode
-    PRUNE_MODE=`echo $INFO | jq .result.pruned`
-    # check headers
-    HEADERS=`echo $INFO | jq .result.headers`
-    # check blocks
-    BLOCKS=`echo $INFO | jq .result.blocks`
-
-    if [ $PRUNE_MODE != "true" ]; then
-      echo "Not pruned"
-      # Node pruned so lets do the switching
-      echo "Checking if synced...."
-      if [ ! -f /statuses/node-status-bitcoind-ready ]; then
-        if [ $HEADERS -eq $BLOCKS ]; then
-            echo "Bitcoind has been switched across to neutrino"
-            touch /statuses/node-status-bitcoind-ready
-            sed -i 's/bitcoin.node\=neutrino/bitcoin.node\=bitcoind/g; ' /lnd/lnd.conf
+      if [ $PRUNE_MODE != "true" ]; then
+        echo "Not pruned"
+        # Node pruned so lets do the switching
+        echo "Checking if synced...."
+        if [ ! -f /statuses/node-status-bitcoind-ready ]; then
+          if [ $HEADERS -eq $BLOCKS ]; then
+              echo "Bitcoind has been switched across to neutrino"
+              touch /statuses/node-status-bitcoind-ready
+              sed -i 's/bitcoin.node\=neutrino/bitcoin.node\=bitcoind/g; ' /lnd/lnd.conf
+          fi
         fi
+      else
+        echo "No need to switch from neutrino in pruned mode"
       fi
     else
-      echo "No need to switch from neutrino in pruned mode"
+      # if bitcoind error
+      echo "Error from bitcoind"
+      echo $ERROR
     fi
   else
     echo "Neutrino mode has been disabled"
