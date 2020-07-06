462b3f2062efe414bf340c23eb2cc4f6fd5c9b9a
nolim1t <hello@nolim1t.co>
2020-07-06 13:22:44 +0700

    add documentation

--- build/neutrino-switcher/switch.sh
+++ build/neutrino-switcher/switch.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -e
 
-PASSWORD=`cat $PWD/secrets/rpcpass.txt`
+# Directories
+# Allow access to /secrets/rpcpass.txt
+# Allow access to LND directory (use /lnd/lnd.conf)
+# Allow access to 'statuses'. /statuses/
 
-INFO=`curl --user lncm:$PASSWORD --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' http://10.254.2.2:18332 2>/dev/null`
+PASSWORD=`cat /secrets/rpcpass.txt`
+JSONRPCURL="http://10.254.2.2:18332"
+
+INFO=`curl --user lncm:$PASSWORD --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' $JSONRPCURL 2>/dev/null`
 
 HEADERS=`echo $INFO | jq .result.headers`
 BLOCKS=`echo $INFO | jq .result.blocks`
 
 if [ $HEADERS -eq $BLOCKS ]; then
     echo "Switching over from bitcoind to neutrino"
-    sed 's/bitcoin.node\=neutrino/bitcoin.node\=bitcoind/g; ' lnd/lnd.conf
+    sed 's/bitcoin.node\=neutrino/bitcoin.node\=bitcoind/g; ' /lnd/lnd.conf
 fi
-
